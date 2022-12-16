1122a6066e20b38add5e8c6641923461dcbf3b1e
Daniel <duedme@gmail.com>
2022-12-16 14:07:49 -0600

    Cambios para frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Wallet } from "fuels";
+import "./App.css";
+// Import the contract factory -- you can find the name in index.ts.
+// You can also do command + space and the compiler will suggest the correct name.
+import { SupplyChainAbi } from "./contracts";
+// The address of the contract deployed the Fuel testnet
+const CONTRACT_ID =
+  "0x3edb96c23766b8504caaff042994efa18460e7ba27f60191394a6bcf5be8d7d8";
+//the private key from createWallet.js
+const WALLET_SECRET =
+  "0xc4a69e0cc4ce1e0b45d25899b3cedced332d193c8a5c706187ffd50aa7591ce6";
+// Create a Wallet from given secretKey in this case
+// The one we configured at the chainConfig.json
+const wallet = Wallet.fromPrivateKey(
+  WALLET_SECRET,
+  "https://node-beta-2.fuel.network/graphql"
+);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
-
 export default App;
